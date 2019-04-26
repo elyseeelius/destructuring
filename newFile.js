df78bf22a2a8393ef694225cf6a2512f41d41bfa
Elysee Elius <elyseeelius@outlook.com>
2019-04-26 00:41:22 -0300

    Delete newFile.js

--- newFile.js
+++
@@ -1,7 +0,0 @@
-// console.log(student)
-// console.log(student.name)
-// console.log(student.school)//dot notation
-for (let index = 0; index < student.length; index++) {
-    const element = student[index];
-    console.log(element);
-}
